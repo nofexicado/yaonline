bf9fd4440839f4904061363ef5644b11c0708efd
Mauricio <nofexicado@gmail.com>
2020-07-12 19:48:18 -0300

    aliminado collect y toArray de los querys scope's

--- app/Commerce.php
+++ app/Commerce.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 
 
 
+
 class Commerce extends Model
 {
     
@@ -22,13 +23,11 @@ class Commerce extends Model
     *@inputs(Commerce)
     *@outputs(Commerce,procesado)
   */
-    public function scopeCommerce($query,$Commerce){
-        
-        if($Commerce!='null')
-        
+    public function scopeCommerce($query,$Commerce){ 
+      if(!empty($Commerce)){
         return $query->where('name', 'LIKE', "%$Commerce%");
     }
-
+  }
     
     /*
     *funcion Scope para hacer la consulta de las categorias a travez de Select
@@ -38,14 +37,9 @@ class Commerce extends Model
     public function scopeCategory($query,$category){
         
         if(!empty($category)){
-
         $categories = Category_departament::where('category_id', $category )->pluck('id'); 
-        $categories= collect($categories);
-        $categories->toArray();
-               
                 return $query->whereIn('category_departament_id',$categories);
         }
-            
     }
 
      /*
@@ -54,16 +48,10 @@ class Commerce extends Model
     *@outputs(departament,procesado)
   */
     public function scopeDepartament($query, $departament){
- 
        if(!empty($departament)){
         $departaments = Category_departament::where('departament_id', $departament )->pluck('id'); 
-            
-        $departaments= collect($departaments);
-        $departaments->toArray();
-               
                 return $query->whereIn('category_departament_id',$departaments);
-            }        
-            
+            }               
     }
 
     
@@ -71,7 +59,9 @@ class Commerce extends Model
 
 
 
-
+// $commerce = Commerce::whereHas('category_departament_id', function (Builder $query) use ($category) {
+//   $query->where('category_id', $category);
+//   })->get();
 
 
 
